4e75456dff95f0e99301905a3d2508659dae0715
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-04-10 15:39:52 +0100

    Style menu for mobile and desktop

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,33 @@
-// In Menu.js
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import './Menu.css';
 
 const Menu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="menu">
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink to="/booking">Booking</NavLink>
-      <NavLink to="/account">Account</NavLink>
-      <NavLink to="/download">Download</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/login">Login/Signup</NavLink>
-      <NavLink to="/why-makaton">Why Makaton</NavLink>
-    </nav>
+    <header className="menu">
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={menuOpen ? 'menu-open' : ''}>
+        <NavLink to="/" activeClassName="active" exact>Home</NavLink>
+        <NavLink to="/booking" activeClassName="active">Booking</NavLink>
+        <NavLink to="/account" activeClassName="active">Account</NavLink>
+        <NavLink to="/download" activeClassName="active">Download</NavLink>
+        <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+        <NavLink to="/why-makaton" activeClassName="active">Why Makaton</NavLink>
+      </nav>
+      <NavLink to="/login-signup" activeClassName="active" className="login-signup">
+        Login/Signup
+      </NavLink>
+    </header>
   );
 };
 
