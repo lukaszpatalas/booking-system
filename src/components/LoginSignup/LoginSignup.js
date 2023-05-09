5c35a0be6862e8e78ec531ee18fccffe25262ed8
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-05-09 19:01:16 +0100

    Saved backup to the cloud

--- src/components/LoginSignup/LoginSignup.js
+++ src/components/LoginSignup/LoginSignup.js
@@ -1,8 +1,58 @@
-import React, { useState } from 'react';
-import './LoginSignup.css';
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useState } from "react";
+import "./LoginSignup.css";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const LoginSignup = () => {
   const [showLogin, setShowLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginMessage, setLoginMessage] = useState("");
+  const [signupMessage, setSignupMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      setLoginMessage("Logged in successfully!");
+      navigate("/home"); // Redirect to the Home page
+    } catch (error) {
+      console.error("Error signing in:", error);
+      setLoginMessage("Login failed: " + error.message);
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      setSignupMessage("Account created successfully!");
+      navigate("/home"); // Redirect to the Home page
+    } catch (error) {
+      console.error("Error creating account:", error);
+      setSignupMessage("Signup failed: " + error.message);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  // ...
+
+  // You can add the {loginMessage} and {signupMessage} within the JSX, where you want to display them.
+  // Example: place them right above the Login and Signup buttons, respectively.
+  
 
   const toggleForm = () => {
     setShowLogin(!showLogin);
@@ -14,13 +64,28 @@ const LoginSignup = () => {
         {showLogin ? (
           <div className="login-box">
             <h2>Log In</h2>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <a className="forgot-password" href="#">
               Forgot your password?
             </a>
-            <button type="submit">Log In</button>
-            <button className="google-login">Log In with Google</button>
+            <button type="submit" onClick={handleLogin}>
+              Log In
+            </button>
+            <button className="google-login" onClick={handleGoogleSignIn}>
+  Log In with Google
+</button>
+
             <p>
               New to Makaton?{' '}
               <span className="toggle-form" onClick={toggleForm}>
@@ -31,10 +96,24 @@ const LoginSignup = () => {
         ) : (
           <div className="signup-box">
             <h2>Create Account</h2>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Create Account</button>
-            <button className="google-signup">Sign up with Google</button>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" onClick={handleSignUp}>
+              Create Account
+            </button>
+            <button className="google-signup" onClick={handleGoogleSignIn}>
+  Sign up with Google
+</button>
             <p>
               Already have an account?{' '}
               <span className="toggle-form" onClick={toggleForm}>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6yiCJqCietLf6W-SDdqOVPH2bFUJRlYw",
+  authDomain: "makaton-booking-system.firebaseapp.com",
+  projectId: "makaton-booking-system",
+  storageBucket: "makaton-booking-system.appspot.com",
+  messagingSenderId: "227866199865",
+  appId: "1:227866199865:web:4648fab253593d16f30ae8",
+  measurementId: "G-GBHXCQLNVJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
