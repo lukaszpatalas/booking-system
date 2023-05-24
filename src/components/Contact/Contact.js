77d5e3acd9560dbf2178fe17fb09c69c0b46a2fc
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-05-24 20:23:11 +0100

    Last commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Contact.css';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Form submitted successfully!');
+    setFormData({
+      fullName: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
+  };
+
   return (
-    <div>
-      {/* Write content for Contact */}
+    <div className="contact-page">
+      <div className="contact-container">
+        <div className="contact-box">
+          <h2>Contact Us</h2>
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <input type="text" name="fullName" placeholder="Full Name" value={formData.fullName} onChange={handleChange} required />
+            <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+            <input type="text" name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} required />
+            <textarea name="message" placeholder="Message" value={formData.message} onChange={handleChange} required />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
