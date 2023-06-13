4066c228b635c13556a2841c14fa54a24cb5ac01
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-06-13 21:04:02 +0100

    Add WhyMakaton and Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
+  const links = [
+    { name: 'Account', to: '/account' },
+    { name: 'Admin', to: '/admin' },
+    { name: 'Booking', to: '/booking' },
+    { name: 'Contact', to: '/contact' },
+    { name: 'Download', to: '/download' },
+    { name: 'LoginSignup', to: '/login-signup' },
+    { name: 'WhyMakaton', to: '/why-makaton' },
+  ];
+
   return (
     <div>
       <Header />
-      {/* Other content for the Home page */}
+      <div className="home-grid">
+        {links.map(link => (
+          <Link to={link.to} className="home-grid-item" key={link.name}>
+            <img src="#" alt={`${link.name} icon`} /> {/* Placeholder image */}
+            <p>{link.name}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/WhyMakaton/WhyMakaton.js
+++ src/components/WhyMakaton/WhyMakaton.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import './WhyMakaton.css';
 
 const WhyMakaton = () => {
   return (
-    <div>
-      {/* Write content for WhyMakaton */}
+    <div className="why-makaton-page">
+      <h1>Why Makaton</h1>
+
+      <h2>A Universal Communication Tool</h2>
+      <p>
+        Makaton is a unique language program that uses symbols, signs, and speech to enable everyone to communicate. 
+        It supports children and adults with communication needs to express themselves effectively. Makaton gives a 
+        voice to those who struggle with verbal communication, making it an empowering tool for individuals from all walks of life.
+      </p>
+
+      <h2>Bridging the Gap in Communication</h2>
+      <p>
+        Not everyone can communicate using traditional language methods, and this can result in feelings of isolation, 
+        frustration, and misunderstanding. Makaton bridges this gap, providing a simple and effective way for everyone to 
+        express their thoughts, needs, and emotions. It's not just about communication – it's about connection.
+      </p>
+
+      <h2>A Multimodal Approach</h2>
+      <p>
+        Makaton uses a multimodal approach to communication, combining speech, signs, and symbols. This makes it a 
+        flexible tool that can be tailored to individual needs. Whether you're a parent, carer, educator, or someone who wants 
+        to improve their communication skills, Makaton provides a valuable resource for connecting on a deeper level.
+      </p>
+
+      <h2>Enhancing Language Development and Cognitive Skills</h2>
+      <p>
+        Makaton isn't just for those who have difficulty with speech. It's a valuable tool for enhancing language 
+        development in children, improving cognitive skills, and supporting the learning of new languages. It's also a useful 
+        tool for teaching people of all ages about the value of inclusive communication.
+      </p>
+
+      <h2>A Global Community</h2>
+      <p>
+        When you learn Makaton, you become part of a global community committed to breaking down communication barriers. 
+        It's used in more than 40 countries worldwide and its symbols are universally recognizable, creating a shared language 
+        across borders.
+      </p>
+
+      <h2>Booking a Makaton Sign Language Course</h2>
+      <p>
+        Our booking system makes it simple for you to embark on your Makaton journey. Whether you're a complete beginner 
+        or want to deepen your knowledge, we offer a range of courses that cater to your needs. Select your preferred course, 
+        schedule it at your convenience, and start communicating in a whole new way.
+      </p>
+
+      <p>
+        Makaton has the power to change lives by opening up the world of communication. It breaks down barriers, fosters 
+        understanding, and brings people together. So why wait? Begin your Makaton journey today and unlock a world of 
+        inclusive communication.
+      </p>
     </div>
   );
 };
